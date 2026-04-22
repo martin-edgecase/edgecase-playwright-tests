c6f4b78da809bbb76e5296120c3350a068b5bed8
Martin Belay <martin@edgecase.sk>
2026-04-22 17:10:02 +0200

    config: add baseURL for relative navigation

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://edgecase.sk',
+    headless: true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -48,6 +49,10 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+
+    use: {
+  baseURL: 'https://edgecase.sk',
+}
 
     /* Test against mobile viewports. */
     // {
